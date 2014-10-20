cf812f8ac4718be6a0fe6a9a51eae93c2c6a2f28
Valentine Toulemonde <valentine.toulemonde@gmail.com>
2014-10-20 15:34:02 -0700

    Update README file and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,46 @@
 Book.create(title: 'Panic', author: 'Lauren Oliver', price: 11.99 , img: 'http://ecx.images-amazon.com/images/I/51LV5SbJjlL._SY160_.jpg')
 Book.create(title: 'Tin Drum', author: 'Gunter Grass', price: 15.99 , img: 'http://ecx.images-amazon.com/images/I/51JklycVjAL._AA160_.jpg')
 Book.create(title: 'Missing You', author: 'Harlan Coben', price: 12.95 , img: 'http://ecx.images-amazon.com/images/I/511MTsFgZIL._SY160_.jpg')
+
+Book.create(
+    title: "Savage Harvest",
+    author: "Carl Hoffman",
+    price: 17.06,
+    img:"http://ecx.images-amazon.com/images/I/513pKorXMDL._SY160_.jpg"
+)
+Book.create(
+    title: "Bark",
+    author: "Lorrie Moore",
+    price: 13.67,
+    img:"http://ecx.images-amazon.com/images/I/51msORtIceL._SY160_.jpg"
+)
+Book.create(
+    title: "Shadow Spell",
+    author: "Nora Roberts",
+    price: 17.80,
+    img:"http://ecx.images-amazon.com/images/I/61EnnEGu98L._SY160_.jpg"
+)
+Book.create(
+    title: "Wild",
+    author: "Cheryl Strayed",
+    price: 11.99,
+    img:"http://ecx.images-amazon.com/images/I/41p1ldx9y-L._SY160_.jpg"
+)
+Book.create(
+    title: "The Chance",
+    author: "Robyn Carr",
+    price: 14.89,
+    img:"http://ecx.images-amazon.com/images/I/51SXGSyMhhL._SY160_.jpg"
+)
+Book.create(
+    title: "The Hit",
+    author: "David Baldacci",
+    price: 22.35,
+    img:"http://ecx.images-amazon.com/images/I/518WBY80bCL._SY160_.jpg"
+)
+Book.create(
+    title: "Grain Brain",
+    author: "Kristin Loberg",
+    price: 11.49,
+    img:"http://ecx.images-amazon.com/images/I/51XQjnD5H2L._SY160_.jpg"
+)
